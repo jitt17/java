27d234a58d10cb656a6398f3b76693e532f87acc
jitt <152983650+jitt17@users.noreply.github.com>
2024-05-23 22:27:24 +0530

    Add files via upload

---
+++ Firstclass.java
@@ -0,0 +1,20 @@
+package firstjava;
+
+public class Firstclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Java");//System is class out is package and println() is ststic function so you can not create object of it 
+		
+		
+		int a=10;
+		int b=20;
+		System.out.println();
+		System.out.println("A: "+a+" B: "+b+" Sum of: "+(a+b));
+		System.out.println("A: "+a+" B: "+b+" Multiplication of: "+(a*b));
+		System.out.println("A: "+a+" B: "+b+" Substraction of: "+(a-b));
+		System.out.println("A: "+a+" B: "+b+" Divition of: "+(float)a/b);
+		
+	}
+
+}
