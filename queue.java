cb2ace90c6e7c94367982be510e81e12f80d9c7f
jitt <152983650+jitt17@users.noreply.github.com>
2024-06-18 22:32:56 +0530

    Add files via upload

---
+++ queue.java
@@ -0,0 +1,22 @@
+package queue;
+
+import java.util.*;
+
+public class queue {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Queue<String> queue = new LinkedList<>();
+
+        // Add elements to the queue
+        queue.add("Gujarat");
+        queue.add("Rajasthan");
+        queue.add("Delhi");
+
+        // Display the elements of the queue
+        System.out.println("Queue: " + queue);
+     
+
+	}
+
+}
